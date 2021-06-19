06ecd8de188d3ed77aad4c2fa665b476975f5c19
hernandw <hernandw@gmail.com>
2021-06-18 23:10:24 -0400

    datos del heroe

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,25 +11,51 @@ $(document).ready(function(){
             success: function(data){
                 
 
-                let nombre = data.name
-                let imagen = data.image.url
-                let peso = data.weight
+                let nombre = data.name;
+                let imagen = data.image.url;
+                let publicado = data.biography.publisher;
+                let conex = data.connections['group-affiliation'];
+                let ocupacion = data.work.occupation;
+                let aparicion = data.biography['first-appearance'];
+                let altura = data.appearance.height;
+                let peso = data.appearance.weight;
+                let alianzas = data.biography.aliases;
+                
 
                 $("#heroInfo").html(`
-                    <div class="text-center">
-                        <h3>${nombre}</h3>
-                        <img src="${imagen}">
-                        <h6>Peso: ${peso}</h6>
+                <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="${imagen}" class="card-img" alt="">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                    <h6 class="card-title">Nombre: ${nombre}</h6>
+                    <p class="card-text">Conexiones: ${conex}</p>
+                    <p class="card-text">Publicado por:  ${publicado}</p>
+                    <hr>
+                    <p class="card-text">Ocupación: ${ocupacion}</p>
+                    <hr>
+                    <p class="card-text">Primera Modificación: ${aparicion}</p>
+                    <hr>
+                    <p class="card-text">Altura: ${altura}</p>
+                    <hr>
+                    <p class="card-text">Pesos: ${peso}</p>
+                    <hr>
+                    <p class="card-text">Alianzas: ${alianzas}</p>
                     </div>
+                  </div>
+                </div>
+              </div>
                 `);
 
                 let estadisticas = []
-                data.stats.forEach(function (s){
+                /* data.stats.forEach(function (s){
                     estadisticas.push({
                         label: s.stat.name,
                         y: s.base_stat
                     });
-                });
+                }); */
                 let config = {
                     animationEnable: true,
                     title: {
